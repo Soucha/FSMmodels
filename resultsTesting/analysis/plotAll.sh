73c55ce1d5feac1167a0646ed43f178931531f26
Michal Soucha <michal.soucha@gmail.com>
2017-11-22 12:40:34 +0000

    Results with visualization of comparison of Testing methods

---
+++ resultsTesting/analysis/plotAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# for type in "DFSM" "Mealy" "Moore" "DFA"; do
+for type in "Mealy" "Moore"; do
+    for ES in 0 1 2; do
+	for P in 5 10; do
+		pdflatex.exe "\def\FSMtype{${type}}\def\Inputs{${P}}\def\ES{${ES}}\input{plotTestingResults.tex}"
+		cp plotTestingResults.pdf ${type}_p${P}_es${ES}.pdf
+	done
+    done
+done
+
